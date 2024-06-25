a317b52cc0a7a5db34b99f2fbabe68c20fbaa697
NEERAJ-DHOUNDIYAL <dhoundiyalneeraj93@gmail.com>
2024-06-26 00:25:38 +0530

    new pages added

---
+++ meet.js
@@ -0,0 +1,150 @@
+const btn=document.querySelector("#jan");
+const btn1=document.querySelector("#feb");
+const btn2=document.querySelector("#mar");
+const btn3=document.querySelector("#apr");
+const btn4=document.querySelector("#may");
+const btn5=document.querySelector("#jun");
+const btn6=document.querySelector("#jul");
+const btn7=document.querySelector("#aug");
+const btn8=document.querySelector("#sep");
+const btn9=document.querySelector("#oct");
+const btn10=document.querySelector("#nov");
+const btn11=document.querySelector("#dec");
+const heading=document.querySelector("#vardate");
+const pic=document.querySelector("#imageid");
+const name=document.querySelector("#meetname");
+const address=document.querySelector("#meetadd");
+const mnumber=document.querySelector("#meetno");
+btn.addEventListener("click",()=>{
+    heading.innerText="JAN 2024";
+    pic.src="./character/jan.jpeg";
+    name.innerText="   LUFFY";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+
+})
+btn1.addEventListener("click",()=>{
+    heading.innerText="FEB 2024";
+    pic.src="./character/feb.jpeg";
+    name.innerText="   ZORO";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn2.addEventListener("click",()=>{
+    heading.innerText="MAR 2024";
+    pic.src="./character/mar.jpeg";
+    name.innerText="   NAMI";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn3.addEventListener("click",()=>{
+    heading.innerText="APR 2024";
+    pic.src="./character/apr.jpeg";
+    name.innerText="   ACE";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn4.addEventListener("click",()=>{
+    heading.innerText="MAY 2024";
+    pic.src="./character/may.jpeg";
+    name.innerText="   USSOP";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn5.addEventListener("click",()=>{
+    heading.innerText="JUN 2024";
+    pic.src="./character/jun.jpeg";
+    name.innerText="   MIHAWK";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn6.addEventListener("click",()=>{
+    heading.innerText="JUL 2024";
+    pic.src="./character/jul.jpeg";
+    name.innerText="   BLACKBEARD";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn7.addEventListener("click",()=>{
+    heading.innerText="AUG 2024";
+    pic.src="./character/aug.jpeg";
+    name.innerText="   LAW";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn8.addEventListener("click",()=>{
+    heading.innerText="SEP 2024";
+    pic.src="./character/sep.jpeg";
+    name.innerText="   YUTA";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn9.addEventListener("click",()=>{
+    heading.innerText="OCT 2024";
+    pic.src="./character/oct.jpeg";
+    name.innerText="   SHANKS";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn10.addEventListener("click",()=>{
+    heading.innerText="NOV 2024";
+    pic.src="./character/nov.jpeg";
+    name.innerText="   NICO ROBIN";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
+btn11.addEventListener("click",()=>{
+    heading.innerText="DEC 2024";
+    pic.src="./character/dec.jpeg";
+    name.innerText="   WHITEBEARD";
+    name.style="color:brown"
+    address.innerText="............abc..............";
+    address.style="color:brown"
+
+    mnumber.innerText="......123";
+    mnumber.style="color:brown";
+})
